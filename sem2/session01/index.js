540ae2226b3eeb153a23c558c9d62f0398ee3ea2
Caroline Barra <barracarol@gmail.com>
2022-02-22 19:16:55 +0000

    innit

---
+++ sem2/session01/index.js
@@ -0,0 +1,11 @@
+console.log("Hello World!");
+var a = 5;
+var b = 3;
+var s1 = "Hello";
+var s2 = "Caroline.";
+console.log(a);
+console.log(a * b);
+console.log(a + b);
+console.log(s1 + s2);
+console.log(s1 + " " + s2);
+console.log("Squared of " + a + " is " + a * a);
